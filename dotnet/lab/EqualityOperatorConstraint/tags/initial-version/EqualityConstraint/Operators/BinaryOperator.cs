530575017041b7ce52de82e7bb30deb3f85ce027
pierre.rebours <pierre.rebours@22abf8a1-6e55-0410-962e-8dadc43923c9>
2008-09-07 10:56:43 +0000

    Change of folder names to match the name of the project.

---
+++ dotnet/lab/EqualityOperatorConstraint/tags/initial-version/EqualityConstraint/Operators/BinaryOperator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnit.Framework.Constraints.Operators
+{
+
+    public abstract class BinaryOperator
+    {
+        public abstract string Description(string lhsName, string rhsName);
+        public abstract bool Evaluate(object lhs, object rhs);
+    }
+
+    public class StaticEqualEqualOperator : BinaryOperator
+    {
+        private readonly IStaticEqualityOperatorProvider provider;
+
+        public StaticEqualEqualOperator(IStaticEqualityOperatorProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        public override string Description(string lhsName, string rhsName)
+        {
+            return string.Format("operator ==({0}, {1})", lhsName, rhsName);
+        }
+
+        public override bool Evaluate(object lhs, object rhs)
+        {          
+            return provider.EvaluateStaticEqualEqualOperator(lhs, rhs);
+        }
+    }
+
+    public class StaticNotEqualOperator : BinaryOperator
+    {
+        private readonly IStaticEqualityOperatorProvider provider;
+
+        public StaticNotEqualOperator(IStaticEqualityOperatorProvider provider)
+        {
+            this.provider = provider;
+        }
+        public override string Description(string lhsName, string rhsName)
+        {
+            return string.Format("operator !=({0}, {1})", lhsName, rhsName);
+        }
+
+        public override bool Evaluate(object lhs, object rhs)
+        {
+            return provider.EvaluateStaticNotEqualOperator(lhs, rhs);
+        }
+    }
+
+    public class EqualOperator : BinaryOperator
+    {
+        public override string Description(string lhsName, string rhsName)
+        {
+            return string.Format("{0}.Equals({1})", lhsName, rhsName);
+        }
+
+        public override bool Evaluate(object lhs, object rhs)
+        {
+            return lhs.Equals(rhs);
+        }
+    }
+
+    public class GetHashCodeOperator : BinaryOperator
+    {
+
+        public override string Description(string lhsName, string rhsName)
+        {
+            return string.Format("{0}.GetHashCode() == {1}.GetHashCode()", lhsName, rhsName);
+        }
+
+        public override bool Evaluate(object lhs, object rhs)
+        {
+            if (lhs == null || rhs == null)
+            {
+                throw new ArgumentNullException();
+            }
+            return lhs.GetHashCode() == rhs.GetHashCode();
+        }
+    }
+}
---
+++ dotnet/lab/EqualityOperatorConstraint/trunk/EqualityConstraint/Operators/ReflectiveStaticEqualityOperatorProvider.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace NUnit.Framework.Constraints.Operators
+{
+  
+    public class ReflectiveStaticEqualityOperatorProvider<T> : BaseStaticEqualityOperatorProvider<T> where T : class
+    {
+
+        private bool? InvokeStaticOperator(T lhs, T rhs, string methodName)
+        {
+            MethodInfo mi = typeof(T).GetMethod(methodName, BindingFlags.Static | BindingFlags.Public, null, new Type[] {
+                typeof(T),
+                typeof(T)},
+               null);
+
+            if (mi != null)
+            {
+                object result = mi.Invoke(null, new object[] { lhs, rhs });
+                if (!(result is bool))
+                {
+                    throw new InvalidCastException();
+                }
+                return (bool)result;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        
+        public override bool EvaluateStaticEqualEqualOperator(T lhs, T rhs)
+        {
+            bool? result = InvokeStaticOperator(lhs, rhs, "op_Equality");
+            if (result == null)
+            {
+                throw new ReflectiveStaticEqualEqualOperatorNotFound(string.Format("operator== is not defined in \"{0}\"", typeof(T).Name));
+            }
+            else
+            {
+                return result.Value;
+            }
+        }
+
+        public override bool EvaluateStaticNotEqualOperator(T lhs, T rhs)
+        {
+            bool? result = InvokeStaticOperator(lhs, rhs, "op_Inequality");
+            if (result == null)
+            {
+                throw new ReflectiveStaticNotEqualOperatorNotFound(string.Format("operator!= is not defined in \"{0}\"", typeof(T).Name));
+            }
+            else
+            {
+                return result.Value;
+            }
+        }
+
+       
+    }
+
+    public abstract class ReflectiveStaticOperatorNotFound : Exception
+    {
+        
+        protected ReflectiveStaticOperatorNotFound(string description) : base(description)
+        {
+            
+        }
+    }
+
+    public class ReflectiveStaticEqualEqualOperatorNotFound : ReflectiveStaticOperatorNotFound
+    {
+
+        public ReflectiveStaticEqualEqualOperatorNotFound(string description) : base(description)
+        {
+
+        }
+    }
+
+    public class ReflectiveStaticNotEqualOperatorNotFound : ReflectiveStaticOperatorNotFound
+    {
+
+        public ReflectiveStaticNotEqualOperatorNotFound(string description)
+            : base(description)
+        {
+
+        }
+    }
+}

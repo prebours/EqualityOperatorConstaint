b175e296b7c6bec0c18b63130963484b7d2de68f
pierre.rebours <pierre.rebours@22abf8a1-6e55-0410-962e-8dadc43923c9>
2010-01-10 20:03:21 +0000

    StringFormatting

---
+++ dotnet/lab/StringFormatting/NumberScalingFormatter.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace StringFormatting
+{
+    public class NumberScalingFormatter : IFormatProvider, ICustomFormatter
+    {
+        private readonly CultureInfo _underlyingCulture;
+        private readonly int _numberScaling;
+
+      
+        public NumberScalingFormatter(int numberScaling, CultureInfo underlyingCulture)
+        {
+            if (numberScaling <= 0)
+            {
+                throw new ArgumentOutOfRangeException("numberScaling");
+            }
+            if (underlyingCulture == null)
+            {
+                throw new ArgumentNullException("underlyingCulture");
+            }
+            _numberScaling = numberScaling;
+            _underlyingCulture = underlyingCulture;
+        }
+
+        #region IFormatProvider Members
+
+        public object GetFormat(Type formatType)
+        {
+            object formatter = null;
+            if (formatType == typeof(ICustomFormatter))
+            {
+                formatter = this;
+            }
+            else
+            {
+                formatter = _underlyingCulture.GetFormat(formatType);
+            }
+            return formatter;
+        }
+
+        #endregion
+
+        #region ICustomFormatter Members
+
+        private object Scale(object arg)
+        {
+            object scaledValue = null;
+
+            if (arg == null)
+            {
+                scaledValue = null;
+            }
+            else if (_numberScaling == 0)
+            {
+                scaledValue = arg;
+            }
+            else
+            {
+                try
+                {
+                    double convertedValue = Convert.ToDouble(arg);
+                    scaledValue = Math.Pow(10, _numberScaling * -3) * convertedValue;
+                }
+                catch (InvalidCastException)
+                {
+
+                }
+            }
+            return scaledValue;
+        }
+
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            StringBuilder formattableString = new StringBuilder("{0");
+            if (!string.IsNullOrEmpty(format))
+            {
+                formattableString.Append(":");
+                formattableString.Append(format);
+            }
+            formattableString.Append("}");
+
+            return string.Format(_underlyingCulture, formattableString.ToString(), Scale(arg));
+        }
+
+        #endregion
+    }
+}
---
+++ dotnet/lab/StringFormatting/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Text;
+using System.Globalization;
+
+namespace StringFormatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo invariantCulture = CultureInfo.InvariantCulture;
+            CultureInfo usCulture = new CultureInfo("en-US");
+            CultureInfo frenchCulture = new CultureInfo("fr-FR");
+
+            // 1billion and 345 millions
+            decimal valueToFormat = 1345278000.346M;
+
+            DisplayFormatting("{0:#,#,,.00}", valueToFormat, invariantCulture);
+            DisplayFormatting("{0:#,#,,.00}", valueToFormat, usCulture);
+            DisplayFormatting("{0:#,#,,.00}", valueToFormat, frenchCulture);
+
+
+            DisplayFormatting("{0:C}", valueToFormat, invariantCulture);
+            DisplayFormatting("{0:C}", valueToFormat, new NumberScalingFormatter(2, invariantCulture));
+            DisplayFormatting("{0:C}", valueToFormat, new NumberScalingFormatter(2, usCulture));
+            DisplayFormatting("{0:C}", valueToFormat, new NumberScalingFormatter(2, frenchCulture));
+        }
+
+      
+        // We write in the Debug Output widow of Visual Studio since Console.Write cannot render properly UNICODE characters
+        // (like €)
+        private static void DisplayFormatting(string format, decimal valueToFormat, IFormatProvider formatProvider)
+        {
+            //culture.NumberFormat.NumberGroupSeparator
+            Console.WriteLine(string.Format(formatProvider, format, valueToFormat));
+            Debug.WriteLine(string.Format(formatProvider, format, valueToFormat));
+        }
+
+        
+
+
+    }
+}
